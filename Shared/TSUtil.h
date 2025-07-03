aee3ece85191bedf3114d8a3e3431d69c6b287e4
zhaonan <35675770+invalidunit@users.noreply.github.com>
2025-07-03 10:37:10 +0800

    Update TSUtil.h

--- Shared/TSUtil.h
+++ Shared/TSUtil.h
@@ -10,8 +10,9 @@ extern NSString* getNSStringFromFile(int fd);
 extern void printMultilineNSString(NSString* stringToPrint);
 extern int spawnRoot(NSString* path, NSArray* args, NSString** stdOut, NSString** stdErr);
 extern void killall(NSString* processName, BOOL softly);
-extern void respring_sb(void);
-extern void respring_bb(void);
+extern void kill_SpringBoard(void);
+extern void kill_backboardd(void);
+extern void kill_cfprefsd(void);
 extern void fetchLatestTrollStoreVersion(void (^completionHandler)(NSString* latestVersion));
 extern void fetchLatestLdidVersion(void (^completionHandler)(NSString* latestVersion));
 
